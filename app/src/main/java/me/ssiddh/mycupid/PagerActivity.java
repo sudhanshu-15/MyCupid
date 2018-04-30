b6234dcbfadf5f84dbb0a2ff469d4e2617e2e080
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-29 21:17:37 -0400

    Init commit, basic project setup

---
+++ app/src/main/java/me/ssiddh/mycupid/PagerActivity.java
@@ -0,0 +1,13 @@
+package me.ssiddh.mycupid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pager);
+    }
+}
